8724332080a2c85de3e04bb4cbe9e8ecb20c725c
jawaldi348 <jawaldi416@gmail.com>
2021-05-12 14:44:13 +0700

    templating modal

--- application/libraries/Layouts.php
+++ application/libraries/Layouts.php
@@ -11,4 +11,14 @@ class Layouts
         $data['content'] = $this->_ci->load->view($template, $data, true);
         $this->_ci->parser->parse('layouts/extends', $data);
     }
+    function modal_form($form, $data)
+    {
+        $data['body'] = $this->_ci->load->view($form, $data, true);
+        $this->_ci->load->view('layouts/modal/form', $data);
+    }
+    function modal_data($form, $data)
+    {
+        $data['body'] = $this->_ci->load->view($form, $data, true);
+        $this->_ci->load->view('layouts/modal/data', $data);
+    }
 }
